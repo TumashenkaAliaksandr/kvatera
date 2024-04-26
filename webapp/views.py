190d011bdcd450074615548e427edf3961df198f
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-04-26 20:37:06 +0300

    added template about, url, view

--- webapp/views.py
+++ webapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'webapp/index-3.html')
+
+def about(request):
+
+    return render(request, 'webapp/about.html')
